31fa281d49c184bd0c70965f2b4df0f8ebfd0120
Sam Gleske <sag47@drexel.edu>
2013-11-25 22:52:23 -0500

    First working test!

--- gIntTest.java
+++ gIntTest.java
@@ -1,17 +1,55 @@
-/*
-   Sam Gleske
-   Mon Nov 25 21:27:13 EST 2013
-   java version "1.7.0_45"
-   Apache Ant(TM) version 1.8.2 compiled on December 3 2011
-   */
-
 import junit.framework.*;
-public class gIntTest extends TestCase {
-  private gInt
-  public static void main(String[] args) {
+
+public class gIntTest extends TestCase
+{
+  private gInt num1;
+  private gInt num2;
+  public gIntTest( String name ) {
+    super( name );
   }
   protected void setUp() {
+    num1 = new gInt(5,7);
+    num2 = new gInt(2,3);
   }
-  public static Test suite() {
+  public void testAdd() {
+    gInt result = num1.add( num2 );
+    Assert.assertEquals( 7, result.real() );
+    Assert.assertEquals( 10, result.imag() );
+  }
+  public static Test suite()
+  {
+    TestSuite suite = new TestSuite();
+    suite.addTest( new gIntTest( "testAdd" ));
+    return suite;
+  }
+  public static void main(String[] args) {
+    String[] caseName = { gIntTest.class.getName() };
+    junit.textui.TestRunner.main( caseName );
   }
 }
+
+
+
+  /*
+  public void testMultiply()
+  {
+  gInt expected= new gInt( -37, 84 );
+  gInt result = gInt1.multiply( gInt2 );
+  Assert.assertEquals( expected, result );
+  }
+
+  public void testNorm()
+  {
+  float expected = 5;
+  float result = gInt1.norm();
+  Assert.assertEquals( expected, result );
+  }
+  public static Test suite()
+  {
+  TestSuite suite= new TestSuite();
+  suite.addTest( new gIntTest( "testAdd" ));
+  //suite.addTest( new gIntTest( "testMultiply" ));
+  //suite.addTest( new gIntTest( "testNorm" ));
+  return suite;
+  }
+  */
