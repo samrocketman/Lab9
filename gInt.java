bb77a94d677820ca8e48f4c1e8e7a53150d28840
Sam Gleske <sag47@drexel.edu>
2013-11-25 19:21:38 -0500

    initial commit 2012-06-15

---
+++ gInt.java
@@ -0,0 +1,10 @@
+/*
+   Solution by Sam Gleske
+   java version "1.6.0_26"
+   Apache Ant version 1.8.0 compiled on May 9 2010
+   */
+
+public class gInt {
+  public static void main(String[] args) {
+  }
+}
