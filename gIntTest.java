f5f77e24d730a96f217bc962804ba59570131991
Sam Gleske <sag47@drexel.edu>
2013-11-25 23:39:48 -0500

    All code and tests working!

--- gIntTest.java
+++ gIntTest.java
@@ -1,4 +1,5 @@
 import junit.framework.*;
+import java.lang.Math;
 
 public class gIntTest extends TestCase
 {
@@ -11,15 +12,38 @@ public class gIntTest extends TestCase
     num1 = new gInt(5,7);
     num2 = new gInt(2,3);
   }
+  public void testReal() {
+    Assert.assertEquals( 5, num1.real() );
+    Assert.assertEquals( 2, num2.real() );
+  }
+  public void testImag() {
+    Assert.assertEquals( 7, num1.imag() );
+    Assert.assertEquals( 3, num2.imag() );
+  }
   public void testAdd() {
     gInt result = num1.add( num2 );
     Assert.assertEquals( 7, result.real() );
     Assert.assertEquals( 10, result.imag() );
   }
+  public void testMultiply() {
+    gInt result = num1.multiply( num2 );
+    Assert.assertEquals( -11, result.real() );
+    Assert.assertEquals( 29, result.imag() );
+  }
+  public void testNorm() {
+    float expected1 = (float)Math.sqrt((5*5)+(7*7));
+    float expected2 = (float)Math.sqrt((2*2)+(3*3));
+    Assert.assertEquals( expected1, num1.norm() );
+    Assert.assertEquals( expected2, num2.norm() );
+  }
   public static Test suite()
   {
     TestSuite suite = new TestSuite();
+    suite.addTest( new gIntTest( "testReal" ));
+    suite.addTest( new gIntTest( "testImag" ));
     suite.addTest( new gIntTest( "testAdd" ));
+    suite.addTest( new gIntTest( "testMultiply" ));
+    suite.addTest( new gIntTest( "testNorm" ));
     return suite;
   }
   public static void main(String[] args) {
