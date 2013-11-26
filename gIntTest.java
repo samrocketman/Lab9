c28c143b200994fb0c4aee139e92f14058212cf8
Sam Gleske <sag47@drexel.edu>
2013-11-25 22:07:53 -0500

    started writing tests

---
+++ gIntTest.java
@@ -0,0 +1,17 @@
+/*
+   Sam Gleske
+   Mon Nov 25 21:27:13 EST 2013
+   java version "1.7.0_45"
+   Apache Ant(TM) version 1.8.2 compiled on December 3 2011
+   */
+
+import junit.framework.*;
+public class gIntTest extends TestCase {
+  private gInt
+  public static void main(String[] args) {
+  }
+  protected void setUp() {
+  }
+  public static Test suite() {
+  }
+}
